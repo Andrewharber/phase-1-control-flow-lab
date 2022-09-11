a96f0882942a2b3197097dbdf3281cfa914d7c90
Andrewharber <harber.andrew@ymail.com>
2022-09-11 15:08:04 -0400

    commit tree 9cfbc59edeb6e3fb8e370bad5b167eaab5a78cc9

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+   if (feet <= "400") 
+    return 'This one is on me!'
+    else if (feet >= "401" && feet <= "2000") 
+      return 'That will be twenty bucks.'
+    else if (feet >= "2001" && feet <= "2500")
+       return 'I will gladly take your thirty bucks.'
+    else if (feet >= "2500")
+        return 'No can do.'
+   
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let NYC;  
+return city === "NYC" ? "Ok, sounds good." : "No go."
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+let message = "Bye.";
+  switch (tip) {
+    case 'generous':
+    message = 'Thank you so much.'
+    break;
+    case 'not as generous':
+    message = 'Thank you.'
+    break;
+    default:
+    console.log('Bye.');
+    break;
+  }
+
+  return message;
 }
